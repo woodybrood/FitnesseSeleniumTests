5a3822915004ca42d3655b4dd3159819aee0521d
Daniel Woodward <woody@Dan-Woodwards-Mac-mini.local>
2012-11-14 22:16:51 -0600

    Removed unnecessary imports.

--- src/fitnesse/pages/FitNessePage.java
+++ src/fitnesse/pages/FitNessePage.java
@@ -6,12 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FitNessePage  {
 
